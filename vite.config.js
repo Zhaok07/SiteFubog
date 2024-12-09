c134e94d5806c32472a040a10eb52d533be1c0f8
Caio Caetano Marques <caiocaetanomarques@gmail.com>
2024-12-09 10:17:20 -0300

    config pronta

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    base: "/teste"
     host: '0.0.0.0', // Permite conexões de qualquer IP
     port: 5173, // Ou qualquer porta que você preferir
     strictPort: true, // Garante que a porta escolhida seja usada, sem tentar outras portas
